8f2a2bb5d2830233ff9deb52f1a427c05f3b8614
zhamid01 <zhamid01@uw.edu>
2020-11-20 22:30:50 -0800

    Inputs

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
@@ -10,15 +11,23 @@ import static org.junit.Assert.*;
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        HashMap<String, Integer> actual = HandleTruth.wordCount("you can't handle the truth");
-        HashMap<String, Integer> expected = getHashMapTest();
-        assertEquals(expected, actual);
-        actual = HandleTruth.wordCount("we will we will rock you");
-        expected = getHashMapTest();
-        assertEquals(expected, actual);
-        actual = HandleTruth.wordCount("you and I I and you");
-        expected = getHashMapTest();
-        assertEquals(expected, actual);
+        String[][] input = {
+                {"you can't handle the truth"}
+        };
+        /*
+        HashMap<String, Integer>[][] expected = {
+                {"the, truth, handle, can't, you", 1}
+        };
+        */
+        for (int i = 0; i < input.length; i++) {
+            String n1 = new String();
+            for (int j = 0; j < input[i].length; j++) {
+                n1 = (input[i][j]);
+            }
+            HashMap<String, Integer> actual = HandleTruth.wordCount(n1);
+            HashMap<String, Integer> expected = getHashMapTest();
+            assertEquals(expected, actual);
+        }
     }
 
     private HashMap<String, Integer> getHashMapTest() {
